02d135f39ad2da67822a91c73550bcb8f92aca49
giva16 <ltpie1@hotmail.com>
2024-03-31 17:06:21 +0200

    Component(Card): Mapped props to respective htmle element

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,25 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 
-const Card = () => {
+const Card = ({ title, location, googleMapsUrl, startDate, endDate, description, imageUrl, imageAlt }) => {
   return (
     <article className='card' role='section' aria-label='travel entry'>
       <div className='card-image-container'>
-        <img src={''} className='card-image' alt={''} />
+        <img src={imageUrl} className='card-image' alt={imageAlt} />
       </div>
       <div className='card-content'>
-        <h2 className='card-country'>
+        <h2 className='card-title'>{title}</h2>
+        <h3 className='card-location'>
           <FontAwesomeIcon className='location-icon' icon={faLocationDot} />
-        </h2>
-        <a className='card-map-link'></a>
-        <h3 className='card-location'></h3>
-        <h4 className='card-date'></h4>
-        <p className='card-summary'></p>
+          {location}
+          <span className='card-map-link'>
+            <a href={googleMapsUrl}>View on Google Maps</a>
+          </span>
+        </h3>
+        <h4 className='card-date'>
+          {startDate} - {endDate}
+        </h4>
+        <p className='card-description'>{description}</p>
       </div>
     </article>
   );
