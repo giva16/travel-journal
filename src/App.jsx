275e943039421ed55341fa2e6f73d941557e29de
giva16 <ltpie1@hotmail.com>
2024-03-31 16:58:23 +0200

    Compoent(App): mapped data to Card component Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import Navbar from './components/Navbar';
+import Card from './components/Card';
+import data from './data';
 
 const App = () => {
-  return <Navbar />;
+  const cards = data.map((entry) => {
+    const key = Math.random().toString(16).slice(2);
+    return <Card key={key} {...entry} />;
+  });
+  return (
+    <>
+      <Navbar />
+      <main>{cards}</main>
+    </>
+  );
 };
 
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,22 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+
 const Card = () => {
   return (
-    <div className='card'>
-      <p>Hello</p>
-    </div>
+    <article className='card' role='section' aria-label='travel entry'>
+      <div className='card-image-container'>
+        <img src={''} className='card-image' alt={''} />
+      </div>
+      <div className='card-content'>
+        <h2 className='card-country'>
+          <FontAwesomeIcon className='location-icon' icon={faLocationDot} />
+        </h2>
+        <a className='card-map-link'></a>
+        <h3 className='card-location'></h3>
+        <h4 className='card-date'></h4>
+        <p className='card-summary'></p>
+      </div>
+    </article>
   );
 };
 
